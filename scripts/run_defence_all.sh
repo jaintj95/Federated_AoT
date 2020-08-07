2f4a215a2e2abf095c5165c4ca3ff3187762ce60
Anupam Mediratta <mediratta@gmail.com>
2020-08-08 00:29:42 +0100

    first commit

--- scripts/run_defence_all.sh
+++ scripts/run_defence_all.sh
@@ -1,7 +1,7 @@
 python3 simulated_averaging.py --fraction 0.1 \
 --lr 0.02 \
 --gamma 0.998 \
---num_nets 10000 \
+--num_nets 1000 \
 --fl_round 250 \
 --part_nets_per_round 100 \
 --local_train_period 2 \
@@ -27,7 +27,7 @@ python3 simulated_averaging.py --fraction 0.1 \
 python3 simulated_averaging.py --fraction 0.1 \
 --lr 0.02 \
 --gamma 0.998 \
---num_nets 10000 \
+--num_nets 1000 \
 --fl_round 250 \
 --part_nets_per_round 100 \
 --local_train_period 2 \
@@ -48,12 +48,12 @@ python3 simulated_averaging.py --fraction 0.1 \
 --poison_type southwest \
 --norm_bound 2 \
 --device=cuda:1 \
-> logs/southwest_vgg16_blackbox_no_defense_log 2>&1
+> logs/southwest_vgg16_blackbox_multi_krum_log 2>&1
 
 python3 simulated_averaging.py --fraction 0.1 \
 --lr 0.02 \
 --gamma 0.998 \
---num_nets 10000 \
+--num_nets 1000 \
 --fl_round 250 \
 --part_nets_per_round 100 \
 --local_train_period 2 \
@@ -74,12 +74,12 @@ python3 simulated_averaging.py --fraction 0.1 \
 --poison_type southwest \
 --norm_bound 2 \
 --device=cuda:1 \
-> logs/southwest_vgg16_blackbox_no_defense_log 2>&1
+> logs/southwest_vgg16_blackbox_norm_clipping_log 2>&1
 
 python3 simulated_averaging.py --fraction 0.1 \
 --lr 0.02 \
 --gamma 0.998 \
---num_nets 10000 \
+--num_nets 1000 \
 --fl_round 250 \
 --part_nets_per_round 100 \
 --local_train_period 2 \
@@ -100,12 +100,12 @@ python3 simulated_averaging.py --fraction 0.1 \
 --poison_type southwest \
 --norm_bound 2 \
 --device=cuda:1 \
-> logs/southwest_vgg16_blackbox_no_defense_log 2>&1
+> logs/southwest_vgg16_blackbox_weak_dp_log 2>&1
 
 python3 simulated_averaging.py --fraction 0.1 \
 --lr 0.02 \
 --gamma 0.998 \
---num_nets 10000 \
+--num_nets 1000 \
 --fl_round 250 \
 --part_nets_per_round 100 \
 --local_train_period 2 \
@@ -126,4 +126,4 @@ python3 simulated_averaging.py --fraction 0.1 \
 --poison_type southwest \
 --norm_bound 2 \
 --device=cuda:1 \
-> logs/southwest_vgg16_blackbox_no_defense_log 2>&1
+> logs/southwest_vgg16_blackbox_rfa_log 2>&1
