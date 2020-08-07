853d96e21583a6e705e828220eaad9b103fe8548
Ubuntu <anupam@anupam.gtksvwozlgqexpi013dyn1mtqf.bx.internal.cloudapp.net>
2020-08-07 19:35:41 +0000

    adding remaining stats

--- scripts/run_defence_nc.sh
+++ scripts/run_defence_nc.sh
@@ -2,7 +2,7 @@ python simulated_averaging.py --fraction 0.1 \
 --lr 0.02 \
 --gamma 0.998 \
 --num_nets 200 \
---fl_round 50 \
+--fl_round 200 \
 --part_nets_per_round 10 \
 --local_train_period 2 \
 --adversarial_local_training_period 2 \
@@ -10,7 +10,7 @@ python simulated_averaging.py --fraction 0.1 \
 --model vgg16 \
 --fl_mode fixed-freq \
 --attacker_pool_size 100 \
---defense_method multi-krum\
+--defense_method norm-clipping \
 --attack_method blackbox \
 --attack_case edge-case \
 --model_replacement False \
@@ -22,4 +22,4 @@ python simulated_averaging.py --fraction 0.1 \
 --poison_type southwest \
 --norm_bound 2 \
 --device=cuda:1 \
-> logs/southwest_vgg16_blackbox_krum_log 2>&1
+> logs/southwest_vgg16_blackbox_no_defense_log 2>&1
