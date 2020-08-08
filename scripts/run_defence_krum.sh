087f91f0f2bbd950279ba7271d6b39f9f096a734
Ubuntu <anupam@anupam.gtksvwozlgqexpi013dyn1mtqf.bx.internal.cloudapp.net>
2020-08-08 22:06:59 +0000

    adding remaining stats

--- scripts/run_defence_krum.sh
+++ scripts/run_defence_krum.sh
@@ -1,14 +1,14 @@
 python simulated_averaging.py --fraction 0.1 \
 --lr 0.02 \
 --gamma 0.998 \
---num_nets 200 \
---fl_round 200 \
+--num_nets 1000 \
+--fl_round 250 \
 --part_nets_per_round 10 \
 --local_train_period 2 \
 --adversarial_local_training_period 2 \
 --dataset cifar10 \
 --model vgg16 \
---fl_mode fixed-freq \
+--fl_mode fixed-pool \
 --attacker_pool_size 100 \
 --defense_method krum \
 --attack_method blackbox \
