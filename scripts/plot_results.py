4dfdd603587d168a97cc0f9fca809aae5cb48a37
Anupam Mediratta <mediratta@gmail.com>
2020-08-18 18:04:00 +0100

    refactoring code

--- scripts/plot_results.py
+++ scripts/plot_results.py
@@ -3,6 +3,7 @@ import plotly.express as pxl
 import csv
 import pandas as pd
 import os
+import json
 
 def plot_data(filename, file_dir='results', destination_dir='images', defence_type=''):
     # Create random data with numpy
@@ -26,19 +27,29 @@ def plot_data(filename, file_dir='results', destination_dir='images', defence_ty
     
 def plot_aggregated_data(filename, file_dir='results', destination_dir='images', defence_type=''):
     # Create random data with numpy
-    df = pd.read_csv(os.path.join(file_dir, filename))
+    obj = json.loads(open(os.path.join(file_dir, filename), 'r').read())
+    output_main = {}
+    output_back = {}
+    final_round = '249'
+    for key in obj:
+        obj[key] = json.loads(obj[key])
+        output_main[key] = obj[key]['main_task_acc'][final_round]
+        output_back[key] = obj[key]['backdoor_acc'][final_round]
+    
+    x_indices = list(output_main.keys())
+    
     
     # Create traces
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df['fl_iter'], y=df['main_task_acc'],
+    fig.add_trace(go.Scatter(x=x_indices, y=list(output_main.values()),
                         mode='lines',
                         name='test'))
-    fig.add_trace(go.Scatter(x=df['fl_iter'], y=df['backdoor_acc'],
+    fig.add_trace(go.Scatter(x=x_indices, y=list(output_back.values()),
                         mode='lines', name='target'))
-    x_title = '# Rounds'
+    x_title = '% Compromised Nodes'
     y_title = 'Success Probability'
     title = 'Defence Type: ' + str(defence_type) 
     fig.update_layout(title=title, xaxis_title=x_title, yaxis_title=y_title)
     fig.show()
-    image_filename = os.path.join(destination_dir, filename[:filename.index('.csv')] + '.png')
+    image_filename = os.path.join(destination_dir, filename[:filename.index('.json')] + '.png')
     fig.write_image(image_filename)
--- wrapper_script.py
+++ wrapper_script.py
@@ -76,6 +76,7 @@ def test_number_of_adversary_nodes():
     results_global = {}
     while idx < 10:
         args_t.attacker_pool_size = (idx + 1) * 10
+        args_t.defense_method = 'norm-clipping'
         results_local = sim.main_func(args_t)
         results_global[str(args_t.attacker_pool_size)] = results_local
         idx += 1
